44e7dc9beca35693467bd93a4e1047d692f968ff
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-04-29 06:16:11 +0200

    First commit.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PrimerConstructors1
+{
+    class Car
+    {
+        public string model;
+
+        public Car()
+        {
+            model = "Mustang"; //ovde kontruktor sluzi za inicijalizaciju pocetnih vrednosti
+        }
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();
+            Console.WriteLine(Ford.model);
+        }
+
+    }
+}
